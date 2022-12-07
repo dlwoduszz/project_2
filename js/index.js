2c62434db3ed7b04c3f899a2ebdcc4df0a0ec293
dlwoduszz <lj_yeon_@naver.com>
2022-12-07 18:06:03 +0900

    22.12.07

---
+++ js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    $(function(){
+
+        // $(window).scroll(function(){
+
+        //     if($(this).scrollDown()){
+        //         $('header').fadeOut();
+        //     }else{
+        //         $('header').fadein();
+        //     }
+
+        // });
+        
+
+
+        $(header>button).click(function(){
+            $(nav).slideToggle();
+        });
+    });
+
+});
