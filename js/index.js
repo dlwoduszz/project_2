cef125b9e89fee2b7ad38aa950f3af47544cee51
dlwoduszz <lj_yeon_@naver.com>
2022-12-12 18:59:02 +0900

    12.12

--- js/index.js
+++ js/index.js
@@ -3,17 +3,19 @@ $(document).ready(function(){
     // 사이드네비게이션
     $("header button").click(function(){
         if($("nav").css('left') == '-630px'){
+            $("header .nav_box").fadeIn('0.5s')
             $("nav").css('left','-300px').css('transition','0.5s');
-            $("header .nav_box").css('background','rgba(0, 0, 0, 0.3)').css('transition','0.5s');
         }else{
-            $("header .nav_box").css('background','none').css('transition','0.5s');
+            $("header .nav_box").fadeOut('0.5s')
             $("nav").css('left','-630px').css('transition','0.5s');                
         }
+
     });
+
     $("header .nav_box").click(function(){
-        $(this).fadeOut().css('display','none');
+        $(this).fadeOut('0.5s');
         $("nav").css('left','-630px').css('transition','0.5s'); 
-    })
+    });
 
 
     $(".section1_btn button").click(function(){        
@@ -23,21 +25,32 @@ $(document).ready(function(){
 
 
     $(".section1_btn button:nth-of-type(1)").click(function(){
-        $("#section1>div:nth-of-type(1)").css('display','block').css('transition','0.5s'); 
-        $("#section1>div:nth-of-type(2)").css('display','none');
-        $("#section1>div:nth-of-type(3)").css('display','none');
+        $(".section1_1").css('display','block').css('transition','0.5s'); 
+        $(".section1_2").css('display','none');
+        $(".section1_3").css('display','none');
         
     })
     $(".section1_btn button:nth-of-type(2)").click(function(){
-        $("#section1>div:nth-of-type(2)").css('display','block').css('transition','0.5s'); 
-        $("#section1>div:nth-of-type(3)").css('display','none');
-        $("#section1>div:nth-of-type(1)").css('display','none');
+        $(".section1_2").css('display','block').css('transition','0.5s'); 
+        $(".section1_3").css('display','none');
+        $(".section1_1").css('display','none');
     })
     $(".section1_btn button:nth-of-type(3)").click(function(){
-        $("#section1>div:nth-of-type(3)").css('display','block').css('transition','0.5s'); 
-        $("#section1>div:nth-of-type(2)").css('display','none');
-        $("#section1>div:nth-of-type(1)").css('display','none');
+        $(".section1_3").css('display','block').css('transition','0.5s'); 
+        $(".section1_2").css('display','none');
+        $(".section1_1").css('display','none');
     })
+
+
+    $(".section2_inner>img").hover(function(){
+        $(this).animate({rotate:'360deg'},1500)
+    });
+
+
+   
+
+
+
 
 
     // $('.banner_inner').css({marginLeft:'-100%', transition:'5s'},function(){
