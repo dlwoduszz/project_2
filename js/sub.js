8ea53c2d3c3a89696df6143f90b000d10166a93d
dlwoduszz <lj_yeon_@naver.com>
2022-12-27 17:05:41 +0900

    22.12.27

--- js/sub.js
+++ js/sub.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 
 
-
     function detectMediaSize() {
 
         if (window.matchMedia("(min-width: 501px)").matches) {
@@ -12,6 +11,7 @@ $(document).ready(function () {
                     $(this).css('opacity', '1')
                     $(this).siblings().css('opacity', '0.3')
                 })
+
             $('.news_slide li').eq(0).click(function () {
                 $('.news_slide').animate({ 'margin-left': '20%' }, 350);
                 news_slide_click;
@@ -27,46 +27,93 @@ $(document).ready(function () {
     
 
 
-                
-
-
-
 
             ///social//
-            setInterval(function(){
-                $('.social_slide li').find('span').toggle()
-                },1800)
-
             
+            
+            $('.social_slide li').hover(function(){
+                $(this).find('span').show();
+            },function(){
+                $(this).find('span').hide();
+            })
+
             var social_slide_click =
             $('.social_slide li').click(function () {
                 $(this).css('opacity','1')
                 $(this).siblings().css('opacity', '0.4')
-                $(this)
             })
 
             
-           
-
+            
             $('.social_slide li').eq(0).click(function () {
-                $('.social_slide').animate({ 'margin-left': '28%' }, 350);
+                $('.social_slide').animate({ 'margin-left': '28%' }, 350,function(){
+                    $('.social_slide li').children('a').removeAttr('href')
+                    $('.social_slide li').eq(0).children('a').attr('href','https://www.facebook.com/chupachups.spain')
+                });
+                
                 social_slide_click;
             })
+
             $('.social_slide li').eq(1).click(function () {
-                $('.social_slide').animate({ 'margin-left': '-36%' }, 350);
+                $('.social_slide').animate({ 'margin-left': '-36%' }, 350,function(){
+                    $('.social_slide li').children('a').removeAttr('href')
+                    $('.social_slide li').eq(1).children('a').attr('href','https://www.instagram.com/chupachups_es')
+                });
                 social_slide_click;
             })
+
             $('.social_slide li').eq(2).click(function () {
-                $('.social_slide').animate({ 'margin-left': '-100%' }, 350);
+                $('.social_slide').animate({ 'margin-left': '-100%' }, 350, function(){
+                    $('.social_slide li').children('a').removeAttr('href')
+                    $('.social_slide li').eq(2).children('a').attr('href','https://twitter.com/chupachups_esp')
+                });
                 social_slide_click;
             })
+
             $('.social_slide li').eq(3).click(function () {
-                $('.social_slide').animate({ 'margin-left': '-164%' }, 350);
+                $('.social_slide').animate({ 'margin-left': '-164%' }, 350,function(){
+                    $('.social_slide li').children('a').removeAttr('href')
+                    $('.social_slide li').eq(3).children('a').attr('href','https://www.youtube.com/user/ChupaChupsEs')
+                });
                 social_slide_click;
             })
             
 
         }
+        
+        
+        else if(window.matchMedia("(max-width: 500px)").matches){
+
+        $('.social_slide li').eq(0).click(function () {
+                $('.social_slide li').eq(0).children('a').attr('href','https://www.facebook.com/chupachups.spain')
+        })
+
+        $('.social_slide li').eq(1).click(function () {
+                $('.social_slide li').eq(1).children('a').attr('href','https://www.instagram.com/chupachups_es')
+        })
+
+        $('.social_slide li').eq(2).click(function () {
+                $('.social_slide li').eq(2).children('a').attr('href','https://twitter.com/chupachups_esp')
+        })
+
+        $('.social_slide li').eq(3).click(function () {
+                $('.social_slide li').eq(3).children('a').attr('href','https://www.youtube.com/user/ChupaChupsEs')
+        })
+        }
+
+
+
+
+        $('.product_container>div').eq(0).find('.about').slideDown(1100);
+        $('.product_container>div').eq(0).find('.about').slideUp(1100);
+
+        $('.product_container>div').click(function(){
+            $(this).find('.about').slideToggle(1100);
+            $(this).siblings().click(function(){
+                $(this).siblings().find('.about').slideUp(1100);
+            });
+        })
+
 
     }
 
